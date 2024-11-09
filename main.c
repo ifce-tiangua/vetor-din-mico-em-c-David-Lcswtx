84c9723176855ddb730200dac166265dc66e63c2
David-Lcswtx <davidlucswtx@gmail.com>
2024-11-09 15:22:01 -0300

    versao1

--- main.c
+++ main.c
@@ -1,9 +1,44 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 int main(){
-
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
-
-    return 0;
+	
+	int *v, a;
+	int i;
+	
+	scanf("%d", &a);
+	
+	v = malloc(a * sizeof(int));
+	
+	if(a <= 0){
+		printf("[vazio]\n");
+	}else{
+	
+	for(i = 0; i < a; i++){
+		scanf("%d", &v[i]);
+	}
+		
+	for(i =  0; i < a; i++){
+		
+		if(i == 0){
+			printf("[");
+		}
+		
+		printf("%d", v[i]);
+		
+		if(i < a - 1){
+			printf(",");
+		}
+	
+		if(i == a){
+			printf("]");
+		}
+	}
+	printf("]\n");
+	
+}
+	
+	free(v);
+	
+	return 0;
 }
