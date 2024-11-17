7e57dd1ae13296a47f8f7c13511e19d7a19395c5
David-Lcswtx <davidlucswtx@gmail.com>
2024-11-17 14:39:20 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,59 +1,41 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main()
-
-{
+int main(){
 	
-	int **m; 
-	int a, b; // a = linha - b = coluna
-	int i, k; // elementos para os loops no for
+	int *v, a;
+	int i;
 	
 	scanf("%d", &a);
-	scanf("%d", &b);
 	
-	if(a <= 0 || b <= 0){
+	v = malloc(a * sizeof(int));
+	
+	if(a <= 0){
+		printf("[vazio]\n");
+	}else{
+	
+	for(i = 0; i < a; i++){
+		scanf("%d", &v[i]);
+	}
 		
-		printf("[matriz vazia]\n");
+	for(i =  0; i < a; i++){
 		
-		return 0;
-	}
-	else{
-	
-	//alocar memoria p matriz
-	m = (int **)malloc(a * sizeof(int *));
-	
-	for(i = 0; i < a; i++){
-		m[i] = malloc(b * sizeof(int));
-	}
-	
-	//acima alocacao - abaixo scan elementos
-	
-	for(i = 0; i < a; i++){
-		for(k = 0; k < b; k++){
-			scanf("%d", &m[i][k]);
+		if(i == 0){
+			printf("[");
 		}
-	}
-	
-	//impressao dos elemnetos da matriz
-	for(i = 0; i < a; i++){
-		for(k = 0; k < b; k++){
-			printf("%d", m[i][k]);
-			if(k < b-1){
-				printf(" ");
-			}
+		
+		printf("%d", v[i]);
+		
+		if(i < a - 1){
+			printf(",");
 		}
-		printf("\n");
-	}
 	
-	//desacolar memoria
-	for(i = 0; i < a; i++){
-		free(m[i]);
 	}
-	free (m);
+	printf("]\n");
+	
+}
+	
+	free(v);
 	
 	return 0;
-	
-	}
-	
 }
